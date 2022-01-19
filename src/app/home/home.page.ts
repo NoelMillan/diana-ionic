9901182e60752b61078b049c82d99bfba1e9f064
NoelMillan <noel.millan.r@hotmail.com>
2022-01-19 10:22:07 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,64 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  randomNum = 0;
+  score = 0;
+  spins = 3;
+  sliderValue = 50;
+
+  constructor(public toastController: ToastController) {
+    this.generateNum()
+  }
+
+  async success() {
+    const toast = await this.toastController.create({
+      message: '¡Has acertado en la diana!, has conseguido 20 puntos',
+      duration: 1500,
+      color: "success"
+    });
+    toast.present();
+  }
+
+  async fail() {
+    const toast = await this.toastController.create({
+      message: 'Te has quedado sin intentos, inténtelo de nuevo',
+      duration: 2000,
+      color: "danger"
+    });
+    toast.present();
+  }
+
+  generateNum(){
+    this.randomNum = Math.floor(Math.random()* 100)+1;
+  }
+
+  reset(){
+    this.score = 0;
+    this.sliderValue = 50;
+    this.spins = 3;
+    this.generateNum()
+  }
+  start(){
+    if(this.spins > 0){
+      console.log(this.sliderValue)
+      if(this.sliderValue == this.randomNum){
+        this.score += 20;
+        this.success()
+      }
+      else if(this.sliderValue >= this.randomNum-10 && this.sliderValue <= this.randomNum+10){
+        if(this.sliderValue > this.randomNum){
+          this.score += 10-(this.sliderValue-this.randomNum)
+        }
+        else{
+          this.score += 10-(this.randomNum-this.sliderValue)
+        }
+      }
+      this.spins -= 1
+      this.generateNum()
+    }
+    else{
+      this.fail()
+    }
+  }
 
 }
